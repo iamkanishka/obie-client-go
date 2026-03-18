21705a836d4c4889a1ed4a710a95db5cb466d5d4
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2026-03-18 12:25:39 +0530

    client.go

---
+++ obie/client.go
@@ -0,0 +1,15 @@
+package obie
+
+type Client struct {
+	BaseURL string
+}
+
+func NewClient(baseURL string) *Client {
+	return &Client{
+		BaseURL: baseURL,
+	}
+}
+
+func (c *Client) Hello() string {
+	return "obie-client"
+}
