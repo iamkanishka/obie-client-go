041514aeefd49c466c3938cf9d3a675cfce74f8c
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2026-04-05 15:25:24 +0530

    Updated version

--- obie.go
+++ obie.go
@@ -51,7 +51,7 @@
 package obie
 
 // Version is the current module version.
-const Version = "1.0.0"
+const Version = "1.0.2"
 
 // SpecVersion is the OBIE Read/Write API specification version targeted by
 // this module.
